c06710ad0df6b780a085639eed93bca44a32cec6
EduardaGoulart <goularteduarda.a@gmail.com>
2018-09-10 12:38:18 -0300

    exercicio 2

--- peso_ideal.py
+++ peso_ideal.py
@@ -3,5 +3,23 @@ Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que
 seguintes fórmulas:
 Para homens: (72.7*h) - 58
 Para mulheres: (62.1*h) - 44.7
-
 """
+
+sexo = input("Digite seu sexo: m para masculino e f para feminino: ")
+h = int(input("Digite sua altura: "))
+
+if sexo == 'm':
+    peso = (72.7 * h) - 58
+else:
+    peso = (62.1 * h) - 44.7
+
+print(peso)
+
+# -------------------------------------------- // ------------------------------------------
+
+def calcula_peso(sexo, h):
+    if sexo == 'm':
+        return (72.7 * h) - 58
+    return (62.1 * h) - 44.7
+
+calcula_peso(sexo, h)
