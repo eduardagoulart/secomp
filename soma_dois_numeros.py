6125bc810d72425663485cf01b4a17b6bfb83c65
EduardaGoulart <goularteduarda.a@gmail.com>
2018-09-10 12:07:28 -0300

    resolvendo o exercicio1

---
+++ soma_dois_numeros.py
@@ -0,0 +1,21 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print(f'O resultado da soma dos dois números é: {numero1+numero2}')
+
+# -------------------------------------------- // ------------------------------------------
+
+
+def soma_funcao(numero1, numero2):
+    return numero1 + numero2
+
+
+print(f'O resultado da soma utilizando função é: {soma_funcao(numero1, numero2)}')
+
+# -------------------------------------------- // ------------------------------------------
+soma_lambda = lambda valor1, valor2: valor1 + valor2
+
+
+print(f'O resultado da soma utilizando lambda é: {soma_lambda(numero1, numero2)}')
+
+# -------------------------------------------- // ------------------------------------------
