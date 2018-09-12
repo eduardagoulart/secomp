5cbbc2eb3fa498070d100337046a29c3b77b4189
eduardagoulart <goularteduarda.a@gmail.com>
2018-09-12 08:29:30 -0300

    soma_dois_numeros.py com f string

--- soma_dois_numeros.py
+++ soma_dois_numeros.py
@@ -1,7 +1,7 @@
 numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 
-print('O resultado da soma dos dois números é: {}'.format(numero1+numero2))
+print(f'O resultado da soma dos dois números é: {numero1+numero2}')
 
 
 # -------------------------------------------- // ------------------------------------------
@@ -11,11 +11,11 @@ def soma_funcao(numero1, numero2):
     return numero1 + numero2
 
 
-print('O resultado da soma utilizando função é: {}'.format(soma_funcao(numero1, numero2)))
+print(f'O resultado da soma utilizando função é: {soma_funcao(numero1, numero2)}')
 
 # -------------------------------------------- // ------------------------------------------
 soma_lambda = lambda valor1, valor2: valor1 + valor2
 
-print('O resultado da soma utilizando lambda é: {}'.format(soma_lambda(numero1, numero2)))
+print(f'O resultado da soma utilizando lambda é: {soma_lambda(numero1, numero2)}')
 
 # -------------------------------------------- // ------------------------------------------
