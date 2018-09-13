c398f88c4c5b91b1b39c66372c15ddf9b6c65254
eduardagoulart <goularteduarda.a@gmail.com>
2018-09-13 13:02:41 -0300

    fatiando string

---
+++ fatiando_string.py
@@ -0,0 +1,25 @@
+string_teste = "essa string será fatiada"
+
+print(f'Tamnho: {len(string_teste)}')
+print('---- '*10)
+
+print(f'Posição: {string_teste[3]}')
+print('---- '*10)
+
+print(string_teste[1:])
+print('---- '*10)
+
+print(string_teste[-1:])
+print('---- '*10)
+
+print(string_teste[:1])
+print('---- '*10)
+
+print(string_teste[:-1])
+print('---- '*10)
+
+print(string_teste[::-1])
+print('---- '*10)
+
+print(string_teste[2:8])
+print('---- '*10)
