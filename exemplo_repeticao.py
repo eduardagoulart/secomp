bdd2fecf7f362960843c9e630ef5f56e3a7092c4
eduardagoulart <goularteduarda.a@gmail.com>
2018-09-13 20:25:18 -0300

    exemplo repetição

---
+++ exemplo_repeticao.py
@@ -0,0 +1,15 @@
+i = 10
+while i != 0:
+    print(i)
+    i -= 1
+
+print('---- '*10)
+
+for i in range(0, 10):
+    print(i)
+
+print('---- '*10)
+
+frutas = ['maca', 'pera', 'uva', 'goiaba', 'banana', 'mamão', 'abacaxi']
+for i in frutas:
+    print(i)
